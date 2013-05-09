25b43336503cf4a5a62d226c38f9c6b414c4532b
James Smith <james@floppy.org.uk>
2013-05-09 13:51:58 +0100

    use a proper local tempfile, and upload to hostname/database/date.sql.bz2

--- lib/hoppler.rb
+++ lib/hoppler.rb
@@ -2,25 +2,34 @@ require 'fog'
 require 'active_support/all'
 require 'dotenv'
 require 'mysql2'
+require 'tempfile'
 
 Dotenv.load(".env", ".mysql.env")
 
 class Hoppler
   def self.perform  
+    hostname = `hostname`.strip
+    
     mysql = Mysql2::Client.new(:host => "localhost", :username => ENV['MYSQL_USERNAME'], :password => ENV['MYSQL_PASSWORD'])    
     
     results = mysql.query("SHOW DATABASES")
     databases = results.map{|row| row['Database']}
     
     databases.each do |database|
-    
-      filename = "backup-#{DateTime.now.strftime("%F")}.sql.bz2"
-    
-      filename = "backup-#{DateTime.now.strftime("%F")}.sql.bz2"
-      system "mysqldump #{database} | bzip2 > #{filename}"
-    
-      dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
-      dir.files.create :key => "#{database}/#{filename}", :body => File.open("/tmp/#{filename}")
+      begin
+        # Dump to tempfile
+        tmpfile = Dir::Tmpname.make_tmpname Dir.tmpdir, nil    
+        dump_cmd = "mysqldump #{database} -u #{ENV['MYSQL_USERNAME']}"
+        dump_cmd << " --password=#{ENV['MYSQL_PASSWORD']}" if ENV['MYSQL_PASSWORD']
+        system "#{dump_cmd} | bzip2 > #{tmpfile}"
+        # Write to rackspace
+        dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+        filename = "#{hostname}/#{database}/#{DateTime.now.strftime("%F")}.sql.bz2"
+        dir.files.create :key => filename, :body => File.open(tmpfile)
+      ensure
+        # Clean up tempfile
+        FileUtils.rm tmpfile if tmpfile && File.exists?(tmpfile)
+      end
     end
   end
   
