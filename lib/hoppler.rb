608b882ef12b54e0adb2629e1fc12bb7b02e91fc
pikesley <github@orgraphone.org>
2013-05-14 15:49:42 +0100

    WOW this is ugly
    But it works. Requires some Chef help to make it useful.

--- lib/hoppler.rb
+++ lib/hoppler.rb
@@ -35,6 +35,63 @@ class Hoppler
       end
     end
   end
+
+  class Cloudfile
+    attr_reader :host, :db, :filename, :key
+    def initialize f
+      @cfile = f
+      @key = f.key
+      bits = @key.split "/"
+      @host = bits[0]
+      @db = bits[1]
+      @filename = bits[2]
+    end
+
+    def download
+      tmpfile = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
+      derp = File.open tmpfile, "w"
+      derp.write @cfile.body
+
+      tmpfile
+    end
+  end
+
+  def self.restore
+    dumps = {}
+    dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+    dir.files.each do |f|
+      c = Cloudfile.new f
+      if c.host == self.hostname
+        if dumps[c.db]
+          if c.key > dumps[c.db].key
+            dumps[c.db] = c
+          end
+        else
+          dumps[c.db] = c
+        end
+      end
+    end
+
+    # Sorry. Really.
+    command = "mysqlshow -u #{ENV['MYSQL_USERNAME']} -p#{ENV['MYSQL_PASSWORD']} | grep -v '\\-\\-\\-' | grep -v Databases | tr -s ' ' ' ' | cut -d ' ' -f 2"
+    existing_dbs = `#{command}`.split
+
+    require 'yaml'
+    y = YAML.load File.open "/root/db.creds.yaml"
+    dumps.each do |key, value|
+      if not existing_dbs.include? key
+        puts "Restoring #{key}"
+        pw = y[key]
+        sql = "create database #{key}; grant all on #{key}.* to '#{key}'@'%' identified by '#{pw}'; flush privileges;"
+        command = "mysql -u #{ENV['MYSQL_USERNAME']} -p#{ENV['MYSQL_PASSWORD']} -e \"%s\"" % [ sql ]
+        `#{command}`
+        bzip = value.download
+        command = "bzcat #{bzip} | mysql -u #{ENV['MYSQL_USERNAME']} -p#{ENV['MYSQL_PASSWORD']} #{key}"
+        `#{command}`
+        File.unlink bzip
+      end
+    end
+  end
   
   def self.cleanup  
     dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
