f06356e9fbba38034a875af9982f2dd43d5cd3e4
pezholio <pezholio@gmail.com>
2013-05-16 11:42:57 +0100

    Add stuff if the DB is remote

--- lib/hoppler.rb
+++ lib/hoppler.rb
@@ -42,6 +42,7 @@ class Hoppler
         # Dump to tempfile
         tmpfile = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
         dump_cmd = "mysqldump #{database} -u #{ENV['MYSQL_USERNAME']} --single-transaction"
+        dump_cmd << " -h #{ENV['MYSQL_HOST']} --set-gtid-purged=OFF" if ENV['MYSQL_HOST'] != "localhost"
         dump_cmd << " --password=#{ENV['MYSQL_PASSWORD']}" if ENV['MYSQL_PASSWORD']
         system "#{dump_cmd} | bzip2 > #{tmpfile}"
         # Write to rackspace
