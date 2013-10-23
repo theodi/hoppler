7524670fe8b5ff8bc3d9f1dda1e47670fb9089bf
pikesley <github@orgraphone.org>
2013-10-23 16:58:46 +0100

    Mongo

--- lib/hoppler.rb
+++ lib/hoppler.rb
@@ -9,21 +9,22 @@ Dotenv.load(".env.test", ".env", ".mysql.env")
 
 class Cloudfile
   attr_reader :host, :db, :filename, :key
+
   def initialize f
-    @cfile = f
-    @key = f.key
-    bits = @key.split "/"
-    @host = bits[0]
-    @db = bits[1]
+    @cfile    = f
+    @key      = f.key
+    bits      = @key.split "/"
+    @host     = bits[0]
+    @db       = bits[1]
     @filename = bits[2]
   end
 
   def download
     tmpfile = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
-    File.open tmpfile, "w" do |derp| 
+    File.open tmpfile, "w" do |derp|
       derp.write @cfile.body
     end
-    
+
     tmpfile
   end
 end
@@ -32,24 +33,75 @@ class Hoppler
 
   def self.hostname
     `hostname`.strip
-  end    
-  
-  def self.backup      
-    results = self.connection.query("show databases")
-    databases = results.map{|row| row['Database']}
-    
+  end
+
+  def self.backup what=nil
+    if not what
+      self.mysql_backup
+    else
+      if what == "mongo"
+        self.mongo_backup
+      end
+    end
+  end
+
+  def self.mongo_backup
+    ENV['MONGO_HOST'] = "localhost" if ENV['MONGO_HOST'].nil?
+    tmpdir  = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
+    tarfile = "mongo.%s.tar.bz2" % [
+        DateTime.now.strftime("%FT%T")
+    ]
+    tarpath = "/tmp/%s" % [
+        tarfile
+    ]
+
+    begin
+      dump_cmd = "mongodump -h %s -o %s > /dev/null" % [
+          ENV['MONGO_HOST'],
+          tmpdir
+      ]
+
+      system dump_cmd
+
+      tar_cmd = "tar cjf %s %s > /dev/null" % [
+          tarpath,
+          tmpdir
+      ]
+
+      system tar_cmd
+
+      dir      = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+      filename = "%s/%s" % [
+          hostname,
+          tarfile
+      ]
+
+      puts filename
+      puts tarpath
+
+      dir.files.create :key => filename, :body => File.open(tarpath)
+    ensure
+      FileUtils.rmtree tmpdir if tmpdir && File.exists?(tmpdir)
+      FileUtils.rm tarpath if tarpath && File.exists?(tarpath)
+    end
+  end
+
+  def self.mysql_backup
+    results   = self.connection.query("show databases")
+    databases = results.map { |row| row['Database'] }
+
     ENV['MYSQL_HOST'] = "localhost" if ENV['MYSQL_HOST'].nil?
-    
+
     databases.each do |database|
       begin
         # Dump to tempfile
-        tmpfile = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
+        tmpfile  = Dir::Tmpname.make_tmpname Dir.tmpdir+File::Separator, nil
         dump_cmd = "mysqldump #{database} -u #{ENV['MYSQL_USERNAME']} --single-transaction"
         dump_cmd << " -h #{ENV['MYSQL_HOST']}"
         dump_cmd << " --password=#{ENV['MYSQL_PASSWORD']}" if ENV['MYSQL_PASSWORD']
         system "#{dump_cmd} | bzip2 > #{tmpfile}"
         # Write to rackspace
-        dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+        dir      = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
         filename = "#{hostname}/#{database}/#{DateTime.now.strftime("%FT%T")}.sql.bz2"
         dir.files.create :key => filename, :body => File.open(tmpfile)
       ensure
@@ -61,7 +113,7 @@ class Hoppler
 
   def self.restore
     dumps = {}
-    dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
+    dir   = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
     dir.files.each do |f|
       c = Cloudfile.new f
       if c.host == self.hostname
@@ -74,12 +126,12 @@ class Hoppler
         end
       end
     end
-    
+
     mysql = self.connection
-    
-    results = mysql.query("show databases")
-    existing_dbs = results.map{|row| row['Database']}
-    
+
+    results      = mysql.query("show databases")
+    existing_dbs = results.map { |row| row['Database'] }
+
     ENV['MYSQL_HOST'] = "localhost" if ENV['MYSQL_HOST'].nil?
 
     if ENV['DB_CREDS_FILE']
@@ -100,7 +152,7 @@ class Hoppler
         mysql.query("flush privileges;")
         bzip = value.download
         dump = `bzcat #{bzip}`
-        
+
         command = "mysql -u #{ENV['MYSQL_USERNAME']} "
         command << " -h #{ENV['MYSQL_HOST']}" if ENV['MYSQL_HOST'] != "localhost"
         command << " -p#{ENV['MYSQL_PASSWORD']}" if ENV['MYSQL_PASSWORD']
@@ -111,25 +163,25 @@ class Hoppler
       end
     end
   end
-  
-  def self.cleanup  
+
+  def self.cleanup
     dir = self.rackspace.directories.get ENV['RACKSPACE_DB_CONTAINER']
-    
+
     dir.files.each do |file|
-      file.destroy if file.key.starts_with?(hostname) && 
-                      file.last_modified < (DateTime.now - 1.month)
+      file.destroy if file.key.starts_with?(hostname) &&
+          file.last_modified < (DateTime.now - 1.month)
     end
   end
-  
+
   def self.rackspace
     Fog::Storage.new({
-        :provider            => 'Rackspace',
-        :rackspace_username  => ENV['RACKSPACE_USERNAME'],
-        :rackspace_api_key   => ENV['RACKSPACE_API_KEY'],
-        :rackspace_region    => ENV['RACKSPACE_REGION'].to_sym
-    })
+                         :provider           => 'Rackspace',
+                         :rackspace_username => ENV['RACKSPACE_USERNAME'],
+                         :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
+                         :rackspace_region   => ENV['RACKSPACE_REGION'].to_sym
+                     })
   end
-  
+
   def self.connection
     ENV['MYSQL_HOST'] = "localhost" if ENV['MYSQL_HOST'].nil?
     client = Mysql2::Client.new(:host => ENV['MYSQL_HOST'], :username => ENV['MYSQL_USERNAME'], :password => ENV['MYSQL_PASSWORD'])
